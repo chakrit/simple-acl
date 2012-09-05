2e0839e0ae3fa90da78bfae3b2cb27842ce42c17
Chakrit Wichian <chakrit@oozou.com>
2012-09-06 00:42:50 +0700

    Implement RedisStore.

--- redis-store.js
+++ redis-store.js
@@ -2,7 +2,39 @@
 // redis-store.js - A redis-backed store for simple-acl
 module.exports = (function() {
 
-  var RedisStore = function() { };
+  var RedisStore = function(client, prefix) {
+    if (!client) { // auto-connect to default host/port
+      client = require('redis').createClient();
+    }
+    if (!prefix) {
+      prefix = 'sacl:';
+    }
+
+    this.client = client;
+    this.prefix = prefix;
+  };
+
+  RedisStore.prototype =
+    { 'grant':
+      function(grantee, resource, callback) {
+        var key = this.prefix + grantee;
+        this.client.sadd(key, resource, callback);
+      }
+
+    , 'assert':
+      function(grantee, resource, callback) {
+        var key = this.prefix + grantee;
+        this.client.sismember(key, resource, function(e, ismember) {
+          callback(e, !!ismember);
+        });
+      }
+
+    , 'revoke':
+      function(grantee, resource, callback) {
+        var key = this.prefix + grantee;
+        this.client.srem(key, resource, callback);
+      }
+    };
 
   return RedisStore;
 
